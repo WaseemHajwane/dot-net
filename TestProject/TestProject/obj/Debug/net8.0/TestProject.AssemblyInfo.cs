a0d85ef71071601a793618c9edc3833ce6a0b852
waseemhajwane <wasim.hajwane1024@gmail.com>
2025-01-14 18:13:59 +0530

    New changes to push

--- TestProject/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ TestProject/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee93bcf48a5c35be78bf22024ba31cab56211295")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
