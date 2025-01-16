28905d9718309fe71d4b722ab277e7780a4aeb52
waseemhajwane <wasim.hajwane1024@gmail.com>
2025-01-16 13:35:05 +0530

    added pubish on IIS server

--- TestProject/TestProject/obj/Release/net8.0/TestProject.AssemblyInfo.cs
+++ TestProject/TestProject/obj/Release/net8.0/TestProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2abb23d45483defaa02891cf2c2b62a46e10cad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c88f8fb0109299584d38cf16c7105ab4cea3de94")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
